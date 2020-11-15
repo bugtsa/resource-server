e05d301085f9787b8765d133a64afff4e4653c7f
Vladimir Polkhovtsev <bugtsa@gmail.com>
2020-11-15 13:26:53 +0300

    update name env password

--- src/main/kotlin/com/bugtsa/casher/resource/CasherResourceServerApplication.kt
+++ src/main/kotlin/com/bugtsa/casher/resource/CasherResourceServerApplication.kt
@@ -18,7 +18,11 @@ fun main(args: Array<String>) {
 private fun getProperties(): Properties {
     val urlDatabase = System.getenv("EXT_DATABASE_URL")
     val userNameDatabase = System.getenv("JDBC_DATABASE_USERNAME")
-    val passwordDatabase = System.getenv("EXT_DATABASE_PASSWORD")
+//    val passwordDatabase = System.getenv("EXT_DATABASE_PASSWORD")
+    val passwordDatabase = System.getenv("EXT_SECOND_DATABASE_PASSWORD")
+//    val urlDatabase = URL_DATABASE
+//    val userNameDatabase = USERNAME_DATABASE
+//    val passwordDatabase = PASSWORD_DATABASE
     val dataBaseConfig = createDataBaseConfig(
             url = urlDatabase,
             userName = userNameDatabase,
@@ -28,7 +32,7 @@ private fun getProperties(): Properties {
         put("spring.datasource.url", dataBaseConfig.url)
         put("spring.datasource.username", dataBaseConfig.userName)
         put("spring.datasource.password", dataBaseConfig.password)
-        put("spring.datasource.driver-class-name", "com.mysql.jdbc.Driver")
+        put("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver")
         put("spring.datasource.tomcat.test-while-idle", false)
         put("spring.datasource.tomcat.validation-query", "SELECT 1")
         put("spring.datasource.initialization-mode", "never")
@@ -38,15 +42,15 @@ private fun getProperties(): Properties {
         put("spring.datasource.hikari.maxLifetime", 1800000)
         put("check-user-scopes", true)
         put("server.port", 9091)
-        put("security.oauth2.resource.jwt.key-value",
-                "    -----BEGIN PUBLIC KEY-----\n" +
-                        "    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgnEzp25qlRJqe/IWf88o\n" +
-                        "    lfkOl9a7tX4wgDrLLzguzMIAHdLv2do1qb48nPJPJYElD6wVTguokOTOND+4oFzJ\n" +
-                        "    Oob/t/6cQ65gPCXVo8OfN1jrternBEIu9t2FuSmn/Ica+JvIZ4NJFz1UnxY1fYGv\n" +
-                        "    WMjsMwpOZk8uU39Pu8ywyyaRPXYi0Sbk8vj35GjdwkhshBOUaE45JlZpBms6HerY\n" +
-                        "    45TBbHTiC9hFQ8slwEd8DIsLYhSlogmCC7hciOVlNEJxUoumq1M20wlizkWhxCyM\n" +
-                        "    Q/5OZMLDBzhK7+lgp9M3ZUdJFOgNKbkR/gdktWC4RZxp90t+Ztct6pHoNz5K/0ji\n" +
-                        "    ZwIDAQAB\n" +
-                        "    -----END PUBLIC KEY-----")
+//        put("security.oauth2.resource.jwt.key-value",
+//                "    -----BEGIN PUBLIC KEY-----\n" +
+//                        "    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgnEzp25qlRJqe/IWf88o\n" +
+//                        "    lfkOl9a7tX4wgDrLLzguzMIAHdLv2do1qb48nPJPJYElD6wVTguokOTOND+4oFzJ\n" +
+//                        "    Oob/t/6cQ65gPCXVo8OfN1jrternBEIu9t2FuSmn/Ica+JvIZ4NJFz1UnxY1fYGv\n" +
+//                        "    WMjsMwpOZk8uU39Pu8ywyyaRPXYi0Sbk8vj35GjdwkhshBOUaE45JlZpBms6HerY\n" +
+//                        "    45TBbHTiC9hFQ8slwEd8DIsLYhSlogmCC7hciOVlNEJxUoumq1M20wlizkWhxCyM\n" +
+//                        "    Q/5OZMLDBzhK7+lgp9M3ZUdJFOgNKbkR/gdktWC4RZxp90t+Ztct6pHoNz5K/0ji\n" +
+//                        "    ZwIDAQAB\n" +
+//                        "    -----END PUBLIC KEY-----")
     }
 }
