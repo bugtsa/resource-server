b8c30b562ae8d9995f7f59a4c1c0dfd32b65976c
Vladimir Polkhovtsev <bugtsa@gmail.com>
2020-12-12 20:12:03 +0300

    add id to payment

--- src/main/kotlin/com/bugtsa/casher/resource/api/models/PaymentDto.kt
+++ src/main/kotlin/com/bugtsa/casher/resource/api/models/PaymentDto.kt
@@ -5,6 +5,7 @@ import com.bugtsa.casher.resource.api.data.res.PaymentRes
 
 class PaymentDto {
 
+    val id: Int
     val userId: String
     val cost: String
     val date: String
@@ -17,30 +18,33 @@ class PaymentDto {
                 cost: String,
                 category: String,
                 date: String) {
+        this.id = INT_DEFAULT_VALUE
         this.userId = userId
         this.cost = cost
         this.date = date
-        this.time = ""
+        this.time = STRING_DEFAULT_VALUE
         this.category = category
-        this.categoryId = 0
-        this.balance = ""
+        this.categoryId = INT_DEFAULT_VALUE
+        this.balance = STRING_DEFAULT_VALUE
     }
 
     constructor(paymentRes: PaymentRes) {
+        id = paymentRes.id
         userId = paymentRes.userId
         cost = paymentRes.cost
         date = paymentRes.date
-        time = ""
+        time = STRING_DEFAULT_VALUE
         category = paymentRes.category
         categoryId = paymentRes.categoryId
-        balance = ""
+        balance = STRING_DEFAULT_VALUE
     }
 
     constructor(payment: Payment) {
+        id = payment.id
         userId = payment.userId
         cost = payment.cost
         date = payment.date
-        time = ""
+        time = STRING_DEFAULT_VALUE
         category = payment.category
         categoryId = payment.categoryId
         balance = payment.balance
@@ -49,6 +53,7 @@ class PaymentDto {
     constructor(oldPayment: Payment,
                 newDate: String,
                 newTime: String) {
+        this.id = oldPayment.id
         this.userId = oldPayment.userId
         this.cost = oldPayment.cost
         this.date = newDate
@@ -59,11 +64,17 @@ class PaymentDto {
     }
 
     override fun toString(): String {
-        return "userId: $userId \n" +
+        return "id: $id \n" +
+                "userId: $userId \n" +
                 "cost: $cost \n" +
                 "date: $date \n" +
                 "time: $time \n" +
                 "category: $category \n" +
                 "balance: $balance"
+    }
+
+    companion object {
+        private const val INT_DEFAULT_VALUE = 0
+        private const val STRING_DEFAULT_VALUE = ""
     }
 }
