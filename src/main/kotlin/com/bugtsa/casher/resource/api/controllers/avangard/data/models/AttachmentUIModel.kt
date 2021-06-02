428d28f73d5091a35cf019e1690aebd7bcc7cea7
Polkhovtsev <polhovcev@dev.vtb>
2021-06-02 08:35:45 +0300

    delete vanguard

--- src/main/kotlin/com/bugtsa/casher/resource/api/controllers/avangard/data/models/AttachmentUIModel.kt
+++
@@ -1,9 +0,0 @@
-package com.bugtsa.casher.resource.api.controllers.avangard.data.models
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class AttachmentUIModel(
-
-        @field:JsonProperty("foto")
-        val foto: String
-)
--- src/main/kotlin/com/bugtsa/casher/resource/api/controllers/avangard/data/models/SendAttachmentUIModel.kt
+++
@@ -1,9 +0,0 @@
-package com.bugtsa.casher.resource.api.controllers.avangard.data.models
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class SendAttachmentUIModel(
-
-        @field:JsonProperty("data")
-        val array: ArrayList<AttachmentUIModel>
-)
